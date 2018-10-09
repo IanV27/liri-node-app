a56e61f240f835eaf0a51c77690ff8d81c1c5d13
IanV27 <ianvillarroel@gmail.com>
2018-10-09 19:22:34 -0400

    initial commit

--- liri.js
+++ liri.js
@@ -27,6 +27,21 @@ switch (action) {
 	break;
 };
 
+function twitter(inputs) {
+	var params = {screen_name: inputs, count: 20};
+	
+		client.get('statuses/user_timeline', params, function(error, tweets, response) {
+			if (!error) {
+				for (i = 0; i < tweets.length; i ++){
+					console.log("Tweet: " + "'" + tweets[i].text + "'" + " Created At: " + tweets[i].created_at);
+				}
+			} else {
+				console.log(error);
+			}
+		});
+
+}
+
 
 
 
