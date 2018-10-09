170da2be4a7280d88295c303f149533b52926a74
IanV27 <ianvillarroel@gmail.com>
2018-10-09 19:20:38 -0400

    initial commit

--- liri.js
+++ liri.js
@@ -1,5 +1,35 @@
 require("dotenv").config();
 var keys = require("./keys");
+var request = require('request');
+var twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var fs = require('fs');
+var client = new Twitter(keys.twitter);
+var input = process.argv;
+var action = input[2];
+var inputs = input[3];
+
+switch (action) {
+	case "my-tweets":
+	twitter(inputs);
+	break;
+
+	case "spotify-this-song":
+	spotify(inputs);
+	break;
+
+	case "movie-this":
+	movie(inputs);
+	break;
+
+	case "do-what-it-says":
+	doit();
+	break;
+};
+
+
+
+
+
 
 var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
